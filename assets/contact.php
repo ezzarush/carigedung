c9b34174a28c4cbee62b24d518aa5e12a9f4733b
shezars <shezars@gmail.com>
2017-09-04 02:14:53 +0700

    Initial Commit

---
+++ assets/contact.php
@@ -0,0 +1,45 @@
+<?php
+// Website Contact Form Generator 
+// http://www.tele-pro.co.uk/scripts/contact_form/ 
+// This script is free to use as long as you  
+// retain the credit link  
+
+// get posted data into local variables
+$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
+$EmailTo = "YOUR EMAIL ADDRESS HERE";
+$Subject = "Message from your site (YOU CAN CHANGE THIS TO WHATEVER YOU WANT)";
+$name = Trim(stripslashes($_POST['name'])); 
+$email = Trim(stripslashes($_POST['email'])); 
+$message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (Trim($EmailFrom)=="") $validationOK=false;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=ENTER A SITE HERE WHERE TO REDIRECT IF SUBMISSION FAILED\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=ENTER A SITE HERE WHERE TO REDIRECT IF SUBMISSION WAS SUCCESSFUL\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=ENTER A SITE HERE WHERE TO REDIRECT IF SUBMISSION FAILED\">";
+}
+?>
